48b62940988f2556cc5e3f29cd8c0f8404f53d54
Mo Al <malbazaz@mba2018.hbs.edu>
2018-10-10 21:17:07 +0000

    automatically backed up by learn

--- src/Timer.js
+++ src/Timer.js
@@ -14,8 +14,11 @@ class Timer extends Component {
 
 
   //Your code here
-shouldComponentUpdate(){
-
+shouldComponentUpdate(nextProp, nextState){
+  if (this.state.time === nextState.time) {
+    return false
+  }
+  return true
 }
 componentDidUpdate(){
   this.timer.current.style.color = '#'+Math.floor(Math.random()*16777215).toString(16)
